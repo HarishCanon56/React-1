3ab1a2eee300917b5a3edb8244178e7b22d53275
HarishCanon56 <148560108+HarishCanon56@users.noreply.github.com>
2023-10-25 19:53:42 +0530

    Add files via upload

---
+++ React startup/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Tone from './openoffer/Tone';
+import Ttwo from './openoffer/Ttwo';
+import Tthree from './openoffer/Tthree';
+
+function App() {
+  return (
+    <div>
+      <Tone />
+      <Ttwo />
+      <Tthree />
+    </div>
+  );
+}
+
+export default App;
---
+++ React startup/Tone.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Tone(){
+	return(
+	<div>
+	<h1>!!Offer!! Price Fluctuation</h1>
+	</div>
+	);
+}
+export default Tone;
